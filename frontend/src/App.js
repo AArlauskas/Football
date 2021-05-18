9d594de38c50c7e10d8ee593b0f43cd2d278bd38
Aurimas <auruxxas.ar@gmail.com>
2021-05-19 01:52:29 +0300

    Initial frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <CssBaseline></CssBaseline>
+    </Router>
+  );
+}
+
+export default App;
