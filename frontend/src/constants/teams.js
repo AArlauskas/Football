2471a3f1099acfb28c0f57d7962a432c1fb8f4b1
Aurimas <auruxxas.ar@gmail.com>
2022-11-12 02:26:00 +0200

    frontend updated team list

--- frontend/src/constants/teams.js
+++ frontend/src/constants/teams.js
@@ -1,99 +1,131 @@
 const teams = {
-  TUR: {
-    url: "https://image.flaticon.com/icons/png/512/197/197518.png",
-    name: "Turkija",
+  QAT: {
+    url: "https://cdn-icons-png.flaticon.com/512/197/197618.png",
+    name: "Kataras",
   },
-  ITA: {
-    url: "https://image.flaticon.com/icons/png/512/323/323325.png",
-    name: "Italija",
+  ECU: {
+    url: "https://cdn-icons-png.flaticon.com/512/197/197588.png",
+    name: "Ekvadoras",
   },
-  WLS: {
-    url: "https://image.flaticon.com/icons/png/512/197/197620.png",
-    name: "Velsas",
-  },
-  CHE: {
-    url: "https://image.flaticon.com/icons/png/512/197/197540.png",
-    name: "Čekija",
-  },
-  DNK: {
-    url: "https://image.flaticon.com/icons/png/512/197/197565.png",
-    name: "Danija",
-  },
-  FIN: {
-    url: "https://image.flaticon.com/icons/png/512/197/197585.png",
-    name: "Suomija",
-  },
-  BEL: {
-    url: "https://image.flaticon.com/icons/png/512/197/197583.png",
-    name: "Belgija",
-  },
-  RUS: {
-    url: "https://image.flaticon.com/icons/png/512/197/197408.png",
-    name: "Rusija",
+  SEN: {
+    url: "https://cdn-icons-png.flaticon.com/512/197/197377.png",
+    name: "Senegalas",
   },
   NLD: {
-    url: "https://image.flaticon.com/icons/png/512/197/197441.png",
+    url: "https://cdn-icons-png.flaticon.com/512/197/197441.png",
     name: "Nyderlandai",
   },
-  UKR: {
-    url: "https://image.flaticon.com/icons/png/512/197/197572.png",
-    name: "Ukraina",
-  },
-  AUS: {
-    url: "https://image.flaticon.com/icons/png/512/197/197447.png",
-    name: "Austrija",
-  },
-  MKD: {
-    url: "https://image.flaticon.com/icons/png/512/197/197413.png",
-    name: "Makedonija",
-  },
   ENG: {
-    url: "https://image.flaticon.com/icons/png/512/197/197485.png",
+    url: "https://cdn-icons-png.flaticon.com/512/197/197374.png",
     name: "Anglija",
   },
-  HRV: {
-    url: "https://image.flaticon.com/icons/png/512/197/197503.png",
-    name: "Kroatija",
+  IRN: {
+    url: "https://cdn-icons-png.flaticon.com/512/197/197574.png",
+    name: "Iranas",
   },
-  SCT: {
-    url: "https://image.flaticon.com/icons/png/512/197/197601.png",
-    name: "Škotija",
+  USA: {
+    url: "https://cdn-icons-png.flaticon.com/512/4628/4628635.png",
+    name: "JAV",
   },
-  CZE: {
-    url: "https://image.flaticon.com/icons/png/512/197/197576.png",
-    name: "Čekija",
+  WLS: {
+    url: "https://cdn-icons-png.flaticon.com/512/197/197620.png",
+    name: "Velsas",
   },
-  ESP: {
-    url: "https://image.flaticon.com/icons/png/512/197/197593.png",
-    name: "Ispanija",
+  ARG: {
+    url: "https://cdn-icons-png.flaticon.com/512/197/197573.png",
+    name: "Argentina",
   },
-  SWE: {
-    url: "https://image.flaticon.com/icons/png/512/197/197564.png",
-    name: "Švedija",
+  SAU: {
+    url: "https://cdn-icons-png.flaticon.com/512/5111/5111777.png",
+    name: "Saudo Arabija",
+  },
+  MEX: {
+    url: "https://cdn-icons-png.flaticon.com/512/5372/5372848.png",
+    name: "Meksika",
   },
   POL: {
-    url: "https://image.flaticon.com/icons/png/512/197/197529.png",
+    url: "https://cdn-icons-png.flaticon.com/512/197/197529.png",
     name: "Lenkija",
   },
-  SVK: {
-    url: "https://image.flaticon.com/icons/png/512/197/197592.png",
-    name: "Slovakija",
-  },
-  HUN: {
-    url: "https://image.flaticon.com/icons/png/512/197/197584.png",
-    name: "Vengrija",
-  },
-  PRT: {
-    url: "https://image.flaticon.com/icons/png/512/197/197463.png",
-    name: "Portugalija",
-  },
   FRA: {
-    url: "https://image.flaticon.com/icons/png/512/197/197560.png",
+    url: "https://cdn-icons-png.flaticon.com/512/197/197560.png",
     name: "Prancūzija",
   },
+  AUS: {
+    url: "https://cdn-icons-png.flaticon.com/512/197/197507.png",
+    name: "Australija",
+  },
+  DNK: {
+    url: "https://cdn-icons-png.flaticon.com/512/197/197565.png",
+    name: "Danija",
+  },
+  TUN: {
+    url: "https://cdn-icons-png.flaticon.com/512/197/197624.png",
+    name: "Tunisas",
+  },
+  ESP: {
+    url: "https://cdn-icons-png.flaticon.com/512/197/197593.png",
+    name: "Ispanija",
+  },
+  CRI: {
+    url: "https://cdn-icons-png.flaticon.com/512/197/197506.png",
+    name: "Kosta Rika",
+  },
   DEU: {
-    url: "https://image.flaticon.com/icons/png/512/197/197571.png",
+    url: "https://cdn-icons-png.flaticon.com/512/197/197571.png",
     name: "Vokietija",
+  },
+  JPN: {
+    url: "https://cdn-icons-png.flaticon.com/512/197/197604.png",
+    name: "Japonija",
+  },
+  BEL: {
+    url: "https://cdn-icons-png.flaticon.com/512/197/197583.png",
+    name: "Belgija",
+  },
+  CAN: {
+    url: "https://cdn-icons-png.flaticon.com/512/197/197430.png",
+    name: "Kanada",
+  },
+  MAR: {
+    url: "https://cdn-icons-png.flaticon.com/512/5372/5372851.png",
+    name: "Marokas",
+  },
+  HRV: {
+    url: "https://cdn-icons-png.flaticon.com/512/197/197503.png",
+    name: "Kroatija",
+  },
+  BRA: {
+    url: "https://cdn-icons-png.flaticon.com/512/197/197386.png",
+    name: "Brazilija",
+  },
+  SRB: {
+    url: "https://cdn-icons-png.flaticon.com/512/197/197602.png",
+    name: "Serbija",
+  },
+  CHE: {
+    url: "https://cdn-icons-png.flaticon.com/512/197/197540.png",
+    name: "Šveicarija",
+  },
+  CMR: {
+    url: "https://cdn-icons-png.flaticon.com/512/197/197531.png",
+    name: "Kamerūnas",
+  },
+  PRT: {
+    url: "https://cdn-icons-png.flaticon.com/512/197/197463.png",
+    name: "Portugalija",
+  },
+  GHA: {
+    url: "https://cdn-icons-png.flaticon.com/512/197/197381.png",
+    name: "Gana",
+  },
+  URY: {
+    url: "https://cdn-icons-png.flaticon.com/512/197/197599.png",
+    name: "Urugvajus",
+  },
+  KOR: {
+    url: "https://cdn-icons-png.flaticon.com/512/197/197582.png",
+    name: "Pietų Korėja",
   },
 };
 
